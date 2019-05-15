602a2d00ba78ba0f9d3434baa5397245ef29494f
minsuYun <yms0214@gmail.com>
2019-05-15 20:59:02 +0900

    dev branch setting

---
+++ all_list/src/App.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+class App extends React.Component {
+  constructor() {
+    super()
+    this.state = {}
+  }
+
+  render () {
+    return (
+      <div>
+      </div>
+    )
+  }
+}
+
+export default App;
