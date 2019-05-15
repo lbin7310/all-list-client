c771aef164eefe9267e17568da038dc5aab06c34
ki_blank <lbin7310@gmail.com>
2019-05-15 21:37:06 +0900

    main.js 레이아웃 작성

--- all_list/src/Component/Main/Main.js
+++ all_list/src/Component/Main/Main.js
@@ -10,12 +10,13 @@ export default class Main extends React.Component {
   render () {
     return (
       <div>
-        <div id="app_title">Every-List</div>
+        <nav id="app_title">Every-List</nav>
         <div className="main_center">
+          <div className="main_username">유저 아이디</div>
           <section className="main_private">
             <div className="main_private_board">
               <div>Private Board</div>
-              <ul> {/* 여기는 개인의 보드를 담는 로직을 만들어야한다. li는 반복한다. */}
+              <ul className="main_boards"> {/* 여기는 개인의 보드를 담는 로직을 만들어야한다. li는 반복한다. */}
                 <li className="boards"> {/* onClick event를 넣는다. 그럼 해당 보더로 이동한다. */}
                   호랑이는 가죽을 남기고,
                   개발자는 TIL를 남긴다.
@@ -24,7 +25,13 @@ export default class Main extends React.Component {
                   인생동안 할 거.
                 </li>
                 <li className="boards">
-                  올해 가장 하고 싶은 것.
+                  올해 가고 싶은 것들.
+                </li >
+                <li className="boards">
+                  올해 갖고 싶은 것들.
+                </li >
+                <li className="boards">
+                  올해 하고 싶은 것들.
                 </li >
               </ul>
             </div>
@@ -32,7 +39,7 @@ export default class Main extends React.Component {
           <section className="main_team">
             <div className="main_team_board">
               <div>Team Board</div>
-              <ul> {/* 팀의 보드를 담는 로직을 만들어야한다. li는 반복한다. */}
+              <ul className="main_boards"> {/* 팀의 보드를 담는 로직을 만들어야한다. li는 반복한다. */}
                 <li className="boards">  {/* onClick event를 넣는다. 그럼 해당 보더로 이동한다. */}
                   축구동아리
                 </li>
