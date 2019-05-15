bc2dfa2a5e05974be30002935428d364d704e468
minsuYun <yms0214@gmail.com>
2019-05-15 23:04:39 +0900

    Feature_Team Commit

--- all_list/src/App.js
+++ all_list/src/App.js
@@ -1,4 +1,5 @@
 import React from "react";
+import Team from "./Component/Team/Team";
 
 class App extends React.Component {
   constructor() {
@@ -9,6 +10,7 @@ class App extends React.Component {
   render () {
     return (
       <div>
+        <Team />
       </div>
     )
   }
