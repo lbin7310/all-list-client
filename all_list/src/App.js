bc62f9330f01681414cf5c8520c06b7c2818b5f6
minsuYun <yms0214@gmail.com>
2019-05-15 20:33:05 +0900

    Feacture_Login Layout Complete

---
+++ all_list/src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import Login from './Component/Login/Login';
+
+class App extends React.Component {
+  constructor () {
+    super();
+    this.state = {};
+  }
+
+  render () {
+    return (
+      <div>
+      </div>
+    );
+  }
+};
+
+export default App;
