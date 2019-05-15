713ee80361e529039b888c6058eedb0ae1d31bb7
ki_blank <lbin7310@gmail.com>
2019-05-15 21:35:50 +0900

    main할 때 App.js

--- all_list/src/App.js
+++ all_list/src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import Main from './Component/Main/Main';
 
 class App extends React.Component {
   constructor () {
@@ -9,6 +10,7 @@ class App extends React.Component {
   render () {
     return (
       <div>
+        <Main />
       </div>
     )
   }
