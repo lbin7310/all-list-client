8faf2d2a1bdbb77390f4cb08a0ef38013796a8d6
minsuYun <yms0214@gmail.com>
2019-05-16 18:03:43 +0900

    Resetting Dev Master after installing React-Router-Dom and Routed Pages

--- all_list/src/App.js
+++ all_list/src/App.js
@@ -1,24 +1,27 @@
-import React from 'react';
-import Login from './Component/Login/Login'
-import Main from './Component/Main/Main';
-import Board from './Component/Board/board'
-import Team from "./Component/Team/Team";
+import React from "react";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import Board from "./Pages/Board";
+import Login from "./Pages/Login";
+import Main from "./Pages/Main";
+import Team from "./Pages/Team";
 
 class App extends React.Component {
-  constructor () {
-    super()
-    this.state = {}
+  constructor() {
+    super();
+    this.state = {};
   }
 
-  render () {
+  render() {
     return (
-      <div>
-        <Login />
-        <Main />
-        <Board />
-        <Team />
-      </div>
-    )
+      <Router>
+        <div>
+          <Route path="/login" exact component={Login}/>
+          <Route path="/main" component={Main}/>
+          <Route path="/board" component={Board}/>
+          <Route path="/team" component={Team}/>
+        </div>
+      </Router>
+    );
   }
 }
 
