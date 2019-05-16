2f75daf20d5c2662cbde0c350ac8fa57af1c39c8
ki_blank <lbin7310@gmail.com>
2019-05-16 20:34:25 +0900

    fakeData 생성

---
+++ all_list/src/fakeData.js
@@ -0,0 +1,54 @@
+export const fakeData = [
+  { userId: '가나다',
+    section: {
+      private_boards: [
+        { board_title: '라마바',
+          board_description: '세종대와 한글창제',
+          board_lists: [
+            { list_title: '사아자',
+              list_cards: {
+                description: [
+                  '차카타파하',
+                  '우와와',
+                  '이야양'
+                ]
+              }
+            },
+            { list_title: '나랏말쌈이',
+              list_cards: {
+                description: [
+                  '궁극에 달아',
+                  '널리 이롭게',
+                  '훈민정음을 전파한다.'
+                ]
+              }
+            }
+          ]
+        },
+        { board_title: '정조',
+          board_description: '신하들아 까불지 마라.',
+          board_lists: [
+            { list_title: '어허, 백성들은 들어라.',
+              list_cards: {
+                description: [
+                  '달을 보고 나를 생각해라.',
+                  '내가 달을 통해 모든 곳을 보고 있으니',
+                  '허튼 짓 하지마라'
+                ]
+              }
+            },
+            { list_title: '어이, 신하들아 나랑 대결하려면 책 좀 읽고와',
+              list_cards: {
+                description: [
+                  '한권을 백번 이상 읽었니?',
+                  '넌 내 적수가 안되.',
+                  '어~ 책 더 읽고와.'
+                ]
+              }
+            }
+          ]
+        }
+      ] // ,한칸 띄우고 teamData넣기.
+    }
+  }
+]
